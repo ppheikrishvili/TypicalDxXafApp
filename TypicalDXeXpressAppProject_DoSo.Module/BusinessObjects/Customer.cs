a88a6840de058336835c8cead1d256fc613003c0
Petre <petre.chitashvili@doso.ge>
2017-03-03 13:04:03 +0000

    რეფაქტორინგი, .Module._Specs პროექტი ტესტებისთვის

--- TypicalDXeXpressAppProject_DoSo.Module/BusinessObjects/Customer.cs
+++ TypicalDXeXpressAppProject_DoSo.Module/BusinessObjects/Customer.cs
@@ -1,10 +1,12 @@
-﻿using DevExpress.ExpressApp.Filtering;
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Filtering;
 using DevExpress.Persistent.Base;
 using DevExpress.Xpo;
 
 namespace TypicalDXeXpressAppProject_DoSo.Module.BusinessObjects
 {
     [DefaultClassOptions]
+    [DefaultListViewOptions(MasterDetailMode.ListViewAndDetailView, true, NewItemRowPosition.Bottom)]
     public class Customer : XPLiteObjectBase
     {
         public Customer(Session session) : base(session) { }
@@ -21,12 +23,5 @@ namespace TypicalDXeXpressAppProject_DoSo.Module.BusinessObjects
         public string Name { get; set; }
         
         public CustomerStatusEnum CustomerStatus { get; set; }
-
-        //[Association]
-        //public XPCollection<Contract> Contracts => GetCollection<Contract>(nameof(Contracts));
-        //[Association]
-        //public XPCollection<Invoice> Invoices => GetCollection<Invoice>(nameof(Invoices));
-        //[Association]
-        //public XPCollection<Transaction> OfficeTransactionsCollection => GetCollection<Transaction>(nameof(TransactionsCollection));
     }
 }
