aaaf754d9bd882bca9f121e02741696bcd809a23
Petre <petre.chitashvili@doso.ge>
2017-03-03 16:11:34 +0000

    კონტრაქტი, ტრანზაქცია, ინვოისი

--- TypicalDXeXpressAppProject_DoSo.Module/BusinessObjects/Customer.cs
+++ TypicalDXeXpressAppProject_DoSo.Module/BusinessObjects/Customer.cs
@@ -7,7 +7,6 @@ using DevExpress.Xpo;
 namespace TypicalDXeXpressAppProject_DoSo.Module.BusinessObjects
 {
     [DefaultClassOptions]
-    [DefaultListViewOptions(MasterDetailMode.ListViewAndDetailView, true, NewItemRowPosition.Bottom)]
     public class Customer : XPLiteObjectBase
     {
         public Customer(Session session) : base(session) { }
@@ -22,7 +21,41 @@ namespace TypicalDXeXpressAppProject_DoSo.Module.BusinessObjects
         [Size(100)]
         [SearchMemberOptions(SearchMemberMode.Include), RuleRequiredField]
         public string Name { get; set; }
-        
+
         public CustomerStatusEnum? CustomerStatus { get; set; }
+
+        [Association]
+        public XPCollection<Contract> Contracts => GetCollection<Contract>(nameof(Contracts));
+        [Association]
+        public XPCollection<Transaction> Transactions => GetCollection<Transaction>(nameof(Transactions));
+        [Association]
+        public XPCollection<Invoice> Invoices => GetCollection<Invoice>(nameof(Invoices));
+    }
+
+    [DefaultClassOptions]
+    public class Contract : XPLiteObjectBase
+    {
+        public Contract(Session session) : base(session) { }
+
+        [Association]
+        public Customer Customer { get; set; }
+    }
+
+    [DefaultClassOptions]
+    public class Transaction : XPLiteObjectBase
+    {
+        public Transaction(Session session) : base(session) { }
+
+        [Association]
+        public Customer Customer { get; set; }
+    }
+
+    [DefaultClassOptions]
+    public class Invoice : XPLiteObjectBase
+    {
+        public Invoice(Session session) : base(session) { }
+
+        [Association]
+        public Customer Customer { get; set; }
     }
 }
