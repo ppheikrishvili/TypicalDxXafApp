6c8f0ca954272b614b71186f43a682ac1392bfd6
ppheikrishvili <paatapp@gmail.com>
2017-03-16 11:01:43 +0000

    Finished ClassLibrary

--- TypicalDXeXpressAppProject_DoSo.Module._Specs/ClassLibrary/StockBalance.cs
+++ TypicalDXeXpressAppProject_DoSo.Module._Specs/ClassLibrary/StockBalance.cs
@@ -3,50 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using TypicalDXeXpressAppProject_DoSo.Module.BusinessObjects;
 
 namespace TypicalDXeXpressAppProject_DoSo.Module._Specs.ClassLibrary
 {
-    public partial class StockTransaction : BaseClass, IBaseClass
+    public partial class StockBalance : BaseClass, IBaseClass
     {
         public int Id
         {
-            get { return GetPropertyValue( () => Id); }
+            get { return GetPropertyValue(() => Id); }
             set { SetPropertyValue(() => Id, value); }
-        }
-
-
-        public DateTime Date
-        {
-            get { return GetPropertyValue( () => Date); }
-            set { SetPropertyValue(() => Date, value); }
         }
 
         public Customer customer
         {
-            get { return GetPropertyValue( () => customer); }
+            get { return GetPropertyValue(() => customer); }
             set { SetPropertyValue(() => customer, value); }
         }
 
         public StockItem stockItem
         {
-            get { return GetPropertyValue( () => stockItem); }
+            get { return GetPropertyValue(() => stockItem); }
             set { SetPropertyValue(() => stockItem, value); }
         }
 
 
         public decimal Amount
         {
-            get { return GetPropertyValue( () => Amount); }
+            get { return GetPropertyValue(() => Amount); }
             set { SetPropertyValue(() => Amount, value); }
         }
 
-
-        public string TransactionNumber
-        {
-            get { return GetPropertyValue( () => TransactionNumber); }
-            set { SetPropertyValue(() => TransactionNumber, value); }
-        }
     }
 }
